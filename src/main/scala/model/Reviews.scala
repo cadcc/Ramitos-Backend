e681d47b5d51cb45bf21402460948a6cd2248f75
Dav1com <42521812+Dav1com@users.noreply.github.com>
2026-05-10 18:55:18 -0400

    feat: added tags to Reviews

--- src/main/scala/model/Reviews.scala
+++ src/main/scala/model/Reviews.scala
@@ -1,7 +1,11 @@
 package cl.cadcc.ramitos.model
 
-import doobie._, doobie.postgres.implicits._
+import cl.cadcc.ramitos.model
+import doobie.*
+import doobie.postgres.implicits.*
+
 import java.time.Instant
+import cl.cadcc.ramitos.model.implicits.given
 
 case class Review(
     id: Int,
@@ -12,11 +16,15 @@ case class Review(
     load: Option[Int],
     utility: Option[Int],
     interest: Option[Int],
+    tags: Vector[String],
     createdAt: Instant
-)
+) derives Read, Write
 
 object Review {
     object Table extends TableDefinition("reviews") {
+        lazy val columns = Composite(all)
+        lazy val columnNames = all.columns.map(_.rawName).toVector
+
         val id: Column[Int] = Column("id")
         val accountId: Column[Int] = Column("account_id")
         val courseCode: Column[String] = Column("course_code")
@@ -25,7 +33,16 @@ object Review {
         val load: Column[Option[Int]] = Column("load")
         val utility: Column[Option[Int]] = Column("utility")
         val interest: Column[Option[Int]] = Column("interest")
+        val tags: Column[Vector[String]] = Column("tags")
         val createdAt: Column[Instant] = Column("created_at")
+
+        def stat(stat: Stat): Column[Option[Int]] =
+            stat match {
+                case model.Stat.Difficulty => difficulty
+                case model.Stat.Load => load
+                case model.Stat.Utility => utility
+                case model.Stat.Interest => interest
+            }
 
         object all extends WithSQLDefinition[Review](Composite((
             id.sqlDef,
@@ -36,6 +53,7 @@ object Review {
             load.sqlDef,
             utility.sqlDef,
             interest.sqlDef,
+            tags.sqlDef,
             createdAt.sqlDef
         ))(Review.apply)(Tuple.fromProductTyped)) with TableDefinition.RowHelpers[Review](this)
     }
---
+++ src/main/scala/repository/ReviewRepository.scala
@@ -0,0 +1,60 @@
+package cl.cadcc.ramitos.repository
+
+import cats.syntax.all.*
+import cats.data.NonEmptyList
+import cl.cadcc.ramitos.model.{Review, Stat}
+import doobie.syntax.all.*
+import doobie.{ConnectionIO, Fragment}
+import fs2.Stream
+
+import scala.language.implicitConversions
+
+trait ReviewRepository {
+    def getById(id: Int): ConnectionIO[Option[Review]]
+    def list(limit: Long, from: Option[Int] = None): Stream[ConnectionIO, Review]
+    def create(
+        accountId: Int,
+        courseCode: String,
+        comments: String,
+        stats: Map[Stat, Int],
+        tags: Vector[String],
+    ): ConnectionIO[Review]
+}
+
+object ReviewRepository {
+
+
+    private class ReviewRepositoryImpl extends ReviewRepository {
+        private val Table = Review.Table
+
+        def getById(id: Int): ConnectionIO[Option[Review]] =
+            sql"SELECT * FROM $Table WHERE ${Table.id === id}".query[Review].option
+
+        def list(limit: Long, from: Option[Int] = None): Stream[ConnectionIO, Review] = {
+            val sql =
+                fr"SELECT * FROM $Table "
+                    ++ from.fold(fr"")(v => fr"WHERE ${Table.id < v} ")
+                ++
+                fr"ORDER BY $Table.id ASC"
+            sql.query[Review].stream
+        }
+
+        def create(
+            accountId: Int,
+            courseCode: String,
+            comments: String,
+            stats: Map[Stat, Int],
+            tags: Vector[String],
+        ): ConnectionIO[Review] = {
+            val upds: NonEmptyList[(Fragment, Fragment)] = NonEmptyList.of(
+                Table.accountId --> accountId,
+                Table.courseCode --> courseCode,
+                Table.comments --> comments
+            )
+
+            val statUpds: List[(Fragment, Fragment)] = stats.toList.map( (stat, value) => Table.stat(stat) --> value.some)
+
+            Table.insertInto(upds ++ statUpds).update.withUniqueGeneratedKeys[Review](Table.columnNames*)
+        }
+    }
+}
