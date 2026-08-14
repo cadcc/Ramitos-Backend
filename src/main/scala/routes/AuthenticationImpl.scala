1f3f89308610b346ea47daa48dbd89bb8d229b70
Dav1com <42521812+Dav1com@users.noreply.github.com>
2026-08-14 20:23:07 +0000

    fix: wrong path in Set-Cookie header

--- src/main/scala/routes/AuthenticationImpl.scala
+++ src/main/scala/routes/AuthenticationImpl.scala
@@ -158,7 +158,7 @@ object Authentication {
                 }.flatten
                 portalUri <- portal.authUri
                 maxAge = dccLoginConfig.loginTimeLimitSeconds * 3
-                cookie = s"WorkflowDccLogin=\"${uuid}\"; Path=/api/workflow/api/dcc; HttpOnly; Max-Age=${maxAge}"
+                cookie = s"WorkflowDccLogin=\"${uuid}\"; Path=/api/workflow/login/dcc; HttpOnly; Max-Age=${maxAge}"
             } yield DccLoginStartOutput(portalUri.toString, cookie)
 
         private val encoder = Base64.getEncoder
